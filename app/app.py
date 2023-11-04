e4307312c7533f07e2fef1c7bc6f0217f244770c
Framor81 <francisco.moralespuente2022@gmail.com>
2023-11-04 13:38:09 -0700

    Calculator

--- app/app.py
+++ app/app.py
@@ -4,14 +4,18 @@ app = Flask(__name__)
 
 @app.route("/login")
 def login():
-       return render_template("index.html", name_of_lists = ["sam", "francisco", "kartika", "billy", "bob"], my_variable = 5)
+       return render_template("login.html", name_of_lists = ["sam", "francisco", "kartika", "billy", "bob"], my_variable = 5)
 
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-       return "<p>home</p>"
+       return "<p>home</p>" 
+
+@app.route('/calculate')
+def calculator_page():
+    return render_template('calculator.html')
 
 
 if __name__ == "__main__":
-       app.run(debug=True)
+       app.run(debug=True)  
