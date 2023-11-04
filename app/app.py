58503a384d2c2a5d4d8010b7f4069e107a319fb9
ksantoso2 <130810746+ksantoso2@users.noreply.github.com>
2023-11-04 13:48:25 -0700

    guest login

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/login")
 def login():
-       return render_template("index.html", name_of_lists = ["sam", "francisco", "kartika", "billy", "bob"], my_variable = 5)
+       # return render_template("index.html", name_of_lists = ["sam", "francisco", "kartika", "billy", "bob"], my_variable = 5)
+       return render_template("login.html")
 
 
 @app.route("/")
